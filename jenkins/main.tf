8b3725fdd42c8e71dda7b047d18223dcb4104c54
Pokaori <sempolina15@gmail.com>
2023-01-16 22:44:01 +0200

    terraform setup

---
+++ jenkins/main.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {
+        default = "us-east-1"
+}
+
+provider "aws" {
+  region     = "${var.region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
+
+resource "aws_security_group" "ingress-all-test" {
+  name = "allow-all-sg-2"
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }// Terraform removes the default rule
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+  }
+    ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 5000
+    to_port = 5000
+    protocol = "tcp"
+  }// Terraform removes the default rule
+  egress {
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "devops"
+  public_key = file("~/.ssh/devops.pub")
+}
+
+resource "aws_instance" "app_server" {
+  ami             = "ami-06878d265978313ca"
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.deployer.id
+  vpc_security_group_ids = ["${aws_security_group.ingress-all-test.id}"]
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+  user_data = file("ubuntu.sh")
+}
---
+++ jenkins/ubuntu.sh
@@ -0,0 +1,94 @@
+#! /bin/bash
+sudo touch /dev/i_worked.txt
+sudo apt update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+sudo apt update
+sudo apt-get install docker-ce -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker ubuntu
+sudo echo "amazon-ecs
+ansicolor
+antisamy-markup-formatter
+artifactory
+aws-codebuild
+aws-credentials
+aws-java-sdk
+aws-parameter-store
+aws-secrets-manager-credentials-provider
+aws-secrets-manager-secret-source
+blueocean
+build-timeout
+build-user-vars-plugin
+code-coverage-api
+codedeploy
+configuration-as-code
+dashboard-view
+description-setter
+docker-java-api
+docker-plugin
+docker-workflow
+ec2
+email-ext
+envinject
+environment-variable-page-decoration
+extended-choice-parameter
+generic-webhook-trigger
+ghprb
+git
+git-client
+git-parameter
+gitlab-plugin:1.6.0
+github
+github-api
+github-branch-source
+http_request
+jacoco
+jdk-tool
+job-dsl
+junit
+mailer
+matrix-project
+parameterized-trigger
+pipeline-aws
+pipeline-input-step
+pipeline-stage-view
+pipeline-utility-steps
+rebuild
+repo
+rich-text-publisher-plugin
+robot
+role-strategy
+saml
+script-security
+simple-theme-plugin
+slack
+sonar
+ssh-agent
+startup-trigger-plugin
+sumologic-publisher
+timestamper
+trilead-api
+warnings-ng
+workflow-aggregator
+ws-cleanup
+xunit" > plugins.txt
+sudo echo "FROM jenkins/jenkins:lts-jdk11
+
+COPY --chown=jenkins:jenkins plugins.txt /plugins.txt
+
+RUN jenkins-plugin-cli --plugin-file /plugins.txt
+
+USER root
+RUN apt-get update && apt-get install -y python3
+RUN apt-get -y install python3-pip
+EXPOSE 5000
+" > custom-jenkins-docker
+
+
+sudo docker build -f custom-jenkins-docker . -t custom-jenkins-image
+
+sudo docker run -p 8080:8080 -p 50000:50000 -p 5000:5000 --restart=on-failure -t custom-jenkins-image
