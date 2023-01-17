9c7da838153b526977eface90ecc65d684088d9b
Pokaori <sempolina15@gmail.com>
2023-01-17 02:25:24 +0200

    change html

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Semonova Polina Devops Project!</p>"
+    return '<h1>My cute project!</h1><img src="https://cataas.com/cat/gif" alt="cat">'
 
 
 app.run(debug=True, host='0.0.0.0')
